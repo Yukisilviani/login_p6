b9a3d6cc8903eef0cad9647904e48a2f03a077c1
Yukisilviani <92262087+Yukisilviani@users.noreply.github.com>
2023-11-19 00:02:11 +0700

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,30 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String uname;
+  HomePage({Key? key, required this.uname}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "Selamat Datang: " + uname,
+                style: TextStyle(
+                  color: Color.fromARGB(255, 71, 140, 225),
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
